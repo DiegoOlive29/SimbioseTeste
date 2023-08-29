ab1c49811d922e36fd333c9256cc125c55a17df1
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2023-08-29 20:09:49 -0300

    Feat: criando a entidade de pessoa

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,6 @@ import userRoutes from "./routes/user.routes";
 const app = express();
 
 app.use(express.json());
-app.use("/users", userRoutes);
+app.use("/pessoa", userRoutes);
 
 export default app;
