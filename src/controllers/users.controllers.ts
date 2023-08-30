1fb6cf45cc7ed9be21dd5ea2e82fd6407f005616
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2023-08-29 22:31:31 -0300

    Feat: Criando arquivos base para a criação das funcionalidade

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,3 +1,32 @@
-import {Request ,Response} from 'express';
+import { Request, Response } from "express";
+import {
+  IPessoaRequest,
+  IPessoaUpdateRequest,
+  IUserResponse,
+} from "../interfaces/users.interfaces";
 
+const createPessoaController = async (req: Request, res: Response) => {
+  return "";
+};
+const listPessoaAllController = async (req: Request, res: Response) => {
+  return "";
+};
+const listPessoaIdController = async (req: Request, res: Response) => {
+  return "";
+};
 
+const updatePessoaController = async (req: Request, res: Response) => {
+  return "";
+};
+
+const deletePessoaController = async (req: Request, res: Response) => {
+  return "";
+};
+
+export {
+  createPessoaController,
+  listPessoaIdController,
+  listPessoaAllController,
+  updatePessoaController,
+  deletePessoaController,
+};
---
+++ src/services/users.services.ts
@@ -0,0 +1,27 @@
+import AppDataSource from "../data-source";
+import { User } from "../entities/user.entity";
+import {
+  IPessoaRequest,
+  IUserResponse,
+  IPessoaUpdateRequest,
+} from "../interfaces/users.interfaces";
+import { AppError } from "../errors/AppError";
+
+export const createUser = async ({}) => {
+  return "";
+};
+
+export const listUserActive = async () => {
+  return "";
+};
+export const listUser = async () => {
+  return "";
+};
+
+export const updateUser = async () => {
+  return "";
+};
+
+export const userDeleteSelfService = async () => {
+  return "";
+};
